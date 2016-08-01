68c3bbab9e44ac12c232c64df4e1c41f84f77c63
imryan <notryancohen@gmail.com>
2016-08-01 14:06:00 -0400

    unhid keys

---
+++ Weather/Keys.swift
@@ -0,0 +1,9 @@
+//
+//  Keys.swift
+//  Weather
+//
+//  Created by Ryan Cohen on 8/1/16.
+//  Copyright © 2016 Ryan Cohen. All rights reserved.
+//
+
+let FORECAST_API_KEY = "8519b654337c222badfac965d878bf04"
